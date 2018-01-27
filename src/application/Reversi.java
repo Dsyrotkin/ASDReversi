88e2b62c7564b46cd9c98ddf45701f105c9d7a77
dindin <dindin0497@sina.com>
2018-01-27 17:40:42 -0600

    first version

---
+++ src/application/Reversi.java
@@ -0,0 +1,49 @@
+package application;
+
+//an experiment to see how much JavaFX code is required
+//to build a game of reversi
+
+//imports
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+//class defnition for reversi game
+public class Reversi extends Application {
+	private StackPane stackPane;
+	private ReversiControl reversiControl;
+
+	// overridden init method
+	public void init() {
+		stackPane = new StackPane();
+		reversiControl = new ReversiControl();
+		stackPane.getChildren().add(reversiControl);
+	}
+	
+	// overridden start method
+	public void start(Stage primaryStage) {
+		primaryStage.setTitle("Reversi");
+		primaryStage.setScene(new Scene(stackPane, 800, 800));
+		primaryStage.show();
+	}
+	
+	// overridden stop method
+	public void stop() {
+		
+	}
+	
+	// entry point into our program for launching our javafx applicaton
+	public static void main(String[] args) {
+		launch(args);
+	}
+	
+	// private fields for a stack pane and a reversi control
+	private StackPane sp_mainlayout;
+	private ReversiControl rc_reversi;
+	
+}
+
+
+
+
---
+++ src/application/ReversiControl.java
@@ -0,0 +1,49 @@
+package application;
+
+import javafx.event.EventHandler;
+import javafx.scene.control.Control;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
+
+/**
+ * Created by Kristijan Pajtasev
+ * 31/10/2017.
+ */
+
+//class definition for a custom reversi control
+class ReversiControl extends Control {
+    private final ReversiBoard reversiBoard;
+
+    // constructor for the class
+    public ReversiControl() {
+        setSkin(new ReversiControlSkin(this));
+        reversiBoard = new ReversiBoard();
+        getChildren().add(reversiBoard);
+
+        setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                reversiBoard.placePiece(event.getX(), event.getY());
+            }
+        });
+
+        // TODO: add press handler which calls reset when space is clicked - pt 4
+        setOnKeyPressed(new EventHandler<KeyEvent>() {
+            @Override
+            public void handle(KeyEvent event) {
+                if(event.getCode() == KeyCode.SPACE) {
+                    reversiBoard.resetGame();
+                }
+            }
+        });
+    }
+
+    // overridden version of the resize method
+    @Override
+    public void resize(double width, double height) {
+        super.resize(width, height);
+        reversiBoard.resize(width, height);
+    }
+
+}
